deee1c789ad9e003b2675991ca5ae1291b259727
Altheo <altheo.pacheco@irc.pshs.edu.ph>
2023-08-07 02:34:57 +0800

    added hamburger

--- main.js
+++ main.js
@@ -1,15 +1,55 @@
-// const scrollDemo = document.querySelector("#hero");
-// const output = document.querySelector("#heroTitle");
 
-// scrollDemo.addEventListener("scroll", event => {
-//     output.innerHTML = `${scrollDemo.scrollTop}`;
+let widthCheck = function() {
+    if (this.window.innerWidth <= 700) {
+        this.document.querySelector('#nav1').style.display = "none";
+        this.document.querySelector('#nav2').style.display = "none";
+        this.document.querySelector('#nav3').style.display = "none";
+        this.document.querySelector('#nav4').style.display = "none";
+        this.document.querySelector('#nav5').style.display = "none";
+        this.document.querySelector('#nav6').style.display = "none";
+        this.document.getElementById('hamburger').style.display = "inline";
+    } else {
+        this.document.querySelector('#nav1').style.display = "inline";
+        this.document.querySelector('#nav2').style.display = "inline";
+        this.document.querySelector('#nav3').style.display = "inline";
+        this.document.querySelector('#nav4').style.display = "inline";
+        this.document.querySelector('#nav5').style.display = "inline";
+        this.document.querySelector('#nav6').style.display = "inline";
+        this.document.getElementById('hamburger').style.display = "none";
+    }
+}
 
-//     if (scrollDemo.scrollTop > 400) {
-//         document.getElementById('navBar').className = 'w-full h-20 fixed top-0 bg-slate-400 transition-colors duration-500 ease-in';
-//     }
+document.addEventListener('load', widthCheck());
 
-// }, { passive: true });
+window.addEventListener('resize', function() {
+    if (this.window.innerWidth <= 700) {
+        this.document.querySelector('#nav1').style.display = "none";
+        this.document.querySelector('#nav2').style.display = "none";
+        this.document.querySelector('#nav3').style.display = "none";
+        this.document.querySelector('#nav4').style.display = "none";
+        this.document.querySelector('#nav5').style.display = "none";
+        this.document.querySelector('#nav6').style.display = "none";
+        this.document.getElementById('hamburger').style.display = "inline";
+    } else {
+        this.document.querySelector('#nav1').style.display = "inline";
+        this.document.querySelector('#nav2').style.display = "inline";
+        this.document.querySelector('#nav3').style.display = "inline";
+        this.document.querySelector('#nav4').style.display = "inline";
+        this.document.querySelector('#nav5').style.display = "inline";
+        this.document.querySelector('#nav6').style.display = "inline";
+        this.document.getElementById('hamburger').style.display = "none";
+        this.document.querySelector('#drop').style.display = 'none';
 
-// function showNav() {
-//     document.getElementById('navBar').style.backgroundColor = 'white';
-// }
+    }
+});
+
+const drop = document.querySelector('#drop');
+
+document.querySelector('#hamburger').addEventListener('click', function() {
+    if (drop.style.display == 'none') {
+        drop.style.backgroundColor = 'white';
+        drop.style.display = 'inline';
+    } else {
+        drop.style.display = 'none';
+    }
+})
