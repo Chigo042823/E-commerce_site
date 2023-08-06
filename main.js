c259edf6bf36a1b1843849b11149619389c6813a
Altheo <altheo.pacheco@irc.pshs.edu.ph>
2023-08-06 20:09:30 +0800

    Move from local directory
    Landing Page layout, no back end functionality yet. Responsive layout issues

---
+++ main.js
@@ -0,0 +1,15 @@
+// const scrollDemo = document.querySelector("#hero");
+// const output = document.querySelector("#heroTitle");
+
+// scrollDemo.addEventListener("scroll", event => {
+//     output.innerHTML = `${scrollDemo.scrollTop}`;
+
+//     if (scrollDemo.scrollTop > 400) {
+//         document.getElementById('navBar').className = 'w-full h-20 fixed top-0 bg-slate-400 transition-colors duration-500 ease-in';
+//     }
+
+// }, { passive: true });
+
+// function showNav() {
+//     document.getElementById('navBar').style.backgroundColor = 'white';
+// }
