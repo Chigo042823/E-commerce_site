f4143a06d5276cb285c7d8d2df04e0a3889f8645
Altheo <altheo.pacheco@irc.pshs.edu.ph>
2023-08-07 22:33:31 +0800

    made navbar disappear
    also added animation to links

--- main.js
+++ main.js
@@ -32,9 +32,27 @@ document.querySelector('#hamburger').addEventListener('click', function() {
     }
 })
 
+let navGone = function() {
+    document.getElementById("navList").style.transition = "transform 0.6s";
+    document.getElementById("navList").style.transitionTimingFunction = "ease-out";
+    document.getElementById("navList").style.transform = "translate(0, -10vh)";
+}
+
+document.addEventListener('scroll', navGone)
+
 document.querySelector('#hero').addEventListener('scroll', function() {
+    navGone();
     drop.style.display = 'none'
 });
+
+let navBack = function() {
+    document.getElementById("navList").style.transition = "transform 0.6s";
+    document.getElementById("navList").style.transitionTimingFunction = "ease-out";
+    document.getElementById("navList").style.transform = "translate(0, 0)";
+}
+
+document.getElementById('navBar').addEventListener('mouseover', navBack)
+document.getElementById('navBar').addEventListener('click', navBack)
 
 let animateHero = function() {
     document.getElementById("heroTitle").style.transition = "transform 1.3s";
